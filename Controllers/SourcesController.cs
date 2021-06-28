4e880faeae60b9260766b351a25b13e67021b4d9
DESKTOP-ODM8L1O\Nhat <nguyenminhnhat020400@gmail.com>
2021-06-28 23:09:13 +0700

    no

--- Controllers/SourcesController.cs
+++ Controllers/SourcesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bigschool.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,7 +18,8 @@ namespace Bigschool.Controllers
 
         public ActionResult Create()
         {
-            var viewmodel = new 
+            var viewmodel = new SourceViewModel { };
+
             return View();
         }
     }
