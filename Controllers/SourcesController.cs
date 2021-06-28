e548a63506a8e90d6d4f3be49752616a0415f9c7
DESKTOP-ODM8L1O\Nhat <nguyenminhnhat020400@gmail.com>
2021-06-28 16:37:46 +0700

    Add project files.

---
+++ Controllers/SourcesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bigschool.Controllers
+{
+    public class SourcesController : Controller
+    {
+
+        // GET: Sources
+        public readonly
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public DbSet<Soure> soures { get; set; }
+    public DbSet<Category> categories { get; set; }
+
+    public ApplicationDbContext()
+        : base("DefaultConnection", throwIfV1Schema: false)
+    {
+    }
+
+    public static ApplicationDbContext Create()
+    {
+        return new ApplicationDbContext();
+    }
+
+}
