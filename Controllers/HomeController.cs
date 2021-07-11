1b0753043e52863d2be64f6c484e1c8ff0269fdb
DESKTOP-ODM8L1O\Nhat <nguyenminhnhat020400@gmail.com>
2021-07-12 02:45:01 +0700

    xong trang 35

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Bigschool.Models;
+using Bigschool.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +11,20 @@ namespace Bigschool.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly ApplicationDbContext _dbContext;
+        
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcommingcourse = _dbContext.soures
+                .Include(c => c.Lecturer)
+                .Include(c => c.catagory)
+                .Where(c => c.DateTime > DateTime.Now).ToList();
+
+            return View(upcommingcourse);
         }
 
         public ActionResult About()
@@ -23,7 +37,6 @@ namespace Bigschool.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
