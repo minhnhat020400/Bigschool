e63ad16caa6f542b5f3dae153f1f53e1fa1c940c
DESKTOP-ODM8L1O\Nhat <nguyenminhnhat020400@gmail.com>
2021-07-12 03:48:53 +0700

    xong trang 40

---
+++ Controllers/AttendancesController.cs
@@ -0,0 +1,32 @@
+﻿using Bigschool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bigschool.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _Dbcontext;
+        public AttendancesController()
+        {
+            _Dbcontext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance 
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _Dbcontext.attendances.Add(attendance);
+            _Dbcontext.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -7,21 +7,31 @@ using System.Data.Entity;
 
 namespace Bigschool.Models
 {
-        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Soure> soures { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Attendance> attendances { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
-            public DbSet<Soure> soures { get; set; }
-            public DbSet<Category> categories { get; set; }
-
-            public ApplicationDbContext()
-                : base("DefaultConnection", throwIfV1Schema: false)
-            {
-
-            }
-
-            public static ApplicationDbContext Create()
-            {
-                return new ApplicationDbContext();
-            }
 
         }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+            .HasRequired(m => m.sourse)
+            .WithMany()
+            .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
 }
